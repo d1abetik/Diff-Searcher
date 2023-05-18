8cbb2d35c9e51d1181384a60f955c65b8c268d15
d1abetik <tapo4ki00@gmail.com>
2023-05-19 01:50:13 +0300

    changes

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -19,22 +19,20 @@ const iter = (obj) => {
   const res = (node, depth) => node.flatMap(({
     key, type, children, children1, children2,
   }) => {
-    if (type === 'nested') {
-      return `${getIndent(depth)}  ${key}: {\n${res(children, depth + 1).join('\n')}\n${getIndent(depth)}${doubleSpace}}`;
+    switch (type){
+      case 'nested':
+        return `${getIndent(depth)}  ${key}: {\n${res(children, depth + 1).join('\n')}\n${getIndent(depth)}${doubleSpace}}`;
+      case 'added':
+        return `${getIndent(depth)}+ ${key}: ${stringifyObj(children, depth)}`;
+      case 'deleted':
+        return `${getIndent(depth)}- ${key}: ${stringifyObj(children, depth)}`;
+      case 'changed':
+        return `${getIndent(depth)}- ${key}: ${stringifyObj(children1, depth)}\n${getIndent(depth)}+ ${key}: ${stringifyObj(children2, depth)}`;
+      case 'unchanged':
+        return `${getIndent(depth)}  ${key}: ${stringifyObj(children, depth)}`;
+      default:
+        return undefined;
     }
-    if (type === 'added') {
-      return `${getIndent(depth)}+ ${key}: ${stringifyObj(children, depth)}`;
-    }
-    if (type === 'deleted') {
-      return `${getIndent(depth)}- ${key}: ${stringifyObj(children, depth)}`;
-    }
-    if (type === 'changed') {
-      return `${getIndent(depth)}- ${key}: ${stringifyObj(children1, depth)}\n${getIndent(depth)}+ ${key}: ${stringifyObj(children2, depth)}`;
-    }
-    if (type === 'unchanged') {
-      return `${getIndent(depth)}  ${key}: ${stringifyObj(children, depth)}`;
-    }
-    return undefined;
   });
   return `{\n${res(obj, 1).join('\n')}\n}\n`;
 };
