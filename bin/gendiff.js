571ba899e6e77104ae0c5b9ba9aa636b3b66470d
d1abetik <tapo4ki00@gmail.com>
2023-05-20 15:23:02 +0300

    changes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,12 +5,13 @@ import objDiff from '../src/index.js';
 const program = new Command();
 
 program
+  .version('0.0.1')
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .version('-V, --version        output the version number')
-  .arguments('<filepath1>', 'type path to your first file')
-  .arguments('<filepath2>', 'type path to your second file')
+  .argument('<filepath1>', 'type path to your first file')
+  .argument('<filepath2>', 'type path to your second file')
   .action((path1, path2) => {
     console.log(objDiff(path1, path2, program.opts().format));
   });
